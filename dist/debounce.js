7a517cacbb5b713fed4e9a209b7a17edeec4dce2
Alex Guo <alexguo@amazon.com>
2022-04-14 22:09:05 -0400

    add debounce

---
+++ dist/debounce.js
@@ -0,0 +1,39 @@
+const debounceImpl = (func, timeout) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const debounce = timeout => {
+  return (value, context) => {
+    if (context.kind === 'method') {
+      return debounceImpl(value, timeout);
+    }
+  };
+};
+
+class ApiClient {
+  constructor(apiContext) {
+    this.apiContext = apiContext;
+  } // @debounce(1000)
+
+
+  post(apiPath, body) {
+    console.log(`Calling ${apiPath} with ${JSON.stringify(body)}`);
+  }
+
+}
+
+const client = new ApiClient({
+  csrfToken: '12345'
+});
+
+for (let i = 0; i < 1000; i++) {
+  client.post('/keywords/recommendations', {
+    campaignId: 'ABCDE'
+  });
+}
